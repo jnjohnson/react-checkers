794202a367d795a583420205e2b0e371c67cca22
Jimmy Johnson <jimmy@jnjohnson.io>
2022-06-08 09:14:28 -0500

    removed default app files, added sass complier, started creating the board

--- src/index.js
+++ src/index.js
@@ -1,17 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './css/index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Piece extends React.Component {
+    isKing = false;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    render() {
+        if (this.props.team === 'red') {
+            return (
+                <div className="piece red"></div>
+            )
+        } else {
+            return (
+                <div className="piece black"></div>
+            )
+        }
+    }
+}
+
+class Square extends React.Component {
+    render() {
+        var piece = '';
+        if (this.props.hasPiece && this.props.row < 3) {
+            piece = <Piece team="red" />
+        } else if (this.props.hasPiece && this.props.row > 4){
+            piece = <Piece team="black" />
+        }
+        return (
+            <button className="square">{piece}</button>
+        );
+    }
+}
+  
+class Board extends React.Component {
+    renderSquare(i, j, hasPiece) {
+        const key = i.toString() + j.toString();
+        return <Square key={key} row={i} column={j} hasPiece={hasPiece} />;
+    }
+  
+    render() {
+        var board = Array(8).fill(Array(8));
+        for (var i=0; i<8; i++) {
+            for (var j=0; j<8; j++) {
+                if (i < 3 || i > 4) {
+                    if ((i%2 === 0 && j%2 === 1) || (i%2 === 1 && j%2 === 0)) {
+                        board[i][j] = this.renderSquare(i, j, true);
+                    } else {
+                        board[i][j] = this.renderSquare(i, j, false);
+
+                    }
+                } else {
+                    board[i][j] = this.renderSquare(i, j, false);
+                }
+            }
+        }
+        return (
+        <div>
+            <div className="column-num">
+            {board.map((row, index) => (
+                <div>{index}</div>
+            ))}
+            </div>
+            {board.map((row, index) => (
+                <div className="row">
+                    <div className="row-num">{index}</div>
+                    {row}
+                </div>
+            ))}
+        </div>
+        );
+    }
+}
+  
+class Game extends React.Component {
+    render() {
+        const status = 'Next player: X';
+        return (
+            <div className="game">
+                <div className="status">{status}</div>
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+  
+  // ========================================
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Game />);
+  
