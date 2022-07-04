09c2872de1788e5f156758d0ec8df5a264b04642
Jimmy Johnson <jimmy@jnjohnson.io>
2022-07-04 09:59:03 -0500

    Added comments, new helper functions, and game logic. Pieces can now move across the board

--- src/game.js
+++ src/game.js
@@ -6,46 +6,124 @@ class Game extends React.Component {
         super(props);
 
         this.state = {
-            board: this.initBoard(),
             blackIsNext: true,
+            board: this.initBoard(),
+            currentPiece: null,
         }
     }
 
-    // Get the board (for checking variables)
+    // --- HELPER FUNCTIONS --- //
+    // - Get the board (for checking variables)
     getBoard() {
         return this.state.board;
     }
-
-    // Get a copy of the board (for setting state)
+    // - Get a copy of the board (for setting state)
     getBoardCopy() {
         return this.state.board.slice();
     }
-
-    // Sets the state of board
+    // - Get a square on the board (for checking variables)
+    getSquare(i, j) {
+        return this.state.board[i][j];
+    }
+    // - Get a copy of a square on the board (for setting state)
+    getSquareCopy(i, j) {
+        return this.state.board.slice()[i][j];
+    }
+    // - Get the current piece being selected
+    getCurrentPiece() {
+        return this.state.currentPiece;
+    }
+    // - Sets the state of board
     setBoard(board) {
         this.setState({
             board: board
         })
     }
-
-    // Get a square on the board (for checking variables)
-    getSquare(i, j) {
-        return this.state.board[i][j];
-    }
-
-    // Get a copy of a square on the board (for setting state)
-    getSquareCopy(i, j) {
-        return this.state.board.slice()[i][j];
-    }
-
-    // Sets the state of a square on the board
+    // - Sets the state of a square on the board
     setSquare(i, j, square) {
         const board = this.getBoardCopy();
         board[i][j] = square;
+        this.setCurrentPiece(i, j, square);
         this.setBoard(board);
     }
+    // - Set the currently selected piece
+    setCurrentPiece(i,j, square) {
+        this.setState({
+            currentPiece: {
+                i: i,
+                j: j,
+                square: square,
+            }
+        });
+    }
+    // - Check to see if the square at i, j has a piece
+    hasPiece(i, j) {
+        return this.state.board[i][j].hasPiece;
+    }
+    // - Check to see if the square at i, j can be moved to
+    canMoveTo(i, j) {
+        return this.state.board[i][j].canMoveTo;
+    }
+    // - Check to see if index i, j is within the bounds of the boad
+    checkIfInBounds(i, j) {
+        if ( i >= 0 && i < this.props.rows && j >= 0 && j < this.props.columns) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // - Returns a list of valid moves
+    getValidMoves(i, j) {
+        var moves = [];
+        const square = this.getSquare(i, j);
+        if (square.piece.isKing) {
+            // TODO: Handle king logic
+        } else if (square.piece.color === 'red') {
+            if (this.checkIfInBounds(i + 1, j + 1) && !this.hasPiece(i + 1, j + 1)) {
+                moves.push({i: i+1, j: j+1});
+            }
+            if (this.checkIfInBounds(i + 1, j - 1) && !this.hasPiece(i + 1, j - 1)) {
+                moves.push({i: i+1, j: j-1});
+            }
+        } else {
+            if (this.checkIfInBounds(i - 1, j + 1) && !this.hasPiece(i - 1, j + 1)) {
+                moves.push({i: i-1, j: j+1});
+            }
+            if (this.checkIfInBounds(i - 1, j - 1) && !this.hasPiece(i - 1, j - 1)) {
+                moves.push({i: i-1, j: j-1});
+            }
+        }
+        return moves;
+    }
+    // - Sets data.clicked and data.canMoveTo to false for every square on the board
+    resetBoard() {
+        const board = this.getBoardCopy();
+        for (var i=0; i<this.props.rows; i++) {
+            for (var j=0; j<this.props.columns; j++) {
+                board[i][j].clicked = false;
+                board[i][j].canMoveTo = false;
+            }
+        }
+        this.setBoard(board);
+        this.setState({
+            currentPiece: null
+        });
+    }
+    // Returns the initial state of a square
+    resetSquare() {
+        return {
+            clicked: false,
+            canMoveTo: false,
+            hasPiece: false,
+            piece: {
+                color: '',
+                isKing: false,
+            }
+        }
+    }
 
-    // Initializes the board
+    // --- INITIALIZATION FUNCTIONS --- //
+    // - Initializes the board
     initBoard() {
         const board = Array(this.props.rows).fill(null).map(row => new Array(this.props.columns).fill(null));
         for (var i=0; i<this.props.rows; i++) {
@@ -55,18 +133,9 @@ class Game extends React.Component {
         }
         return board;
     }
-
-    // Initializes the data for a square on the board 
+    // - Initializes the data for a square on the board 
     initSquare(i, j) {
-        const data = {
-            clicked: false,
-            canMoveTo: false,
-            hasPiece: false,
-            piece: {
-                color: '',
-                isKing: false,
-            }
-        }
+        const data = this.resetSquare();
         if ((i%2 === 1 && j%2 === 0) || (i%2 === 0 && j%2 === 1)) {
             if (i < 3) {
                 data.hasPiece = true;
@@ -79,57 +148,43 @@ class Game extends React.Component {
         return data;
     }
 
+    // --- MISCELLANEOUS FUNCTIONS --- //
+    // Resets the old position of the piece and sets the new position
+    movePieceTo(i, j) {
+        const piece = this.getCurrentPiece();
+        const board = this.getBoardCopy();
+        board[piece.i][piece.j] = this.resetSquare(piece.i, piece.j);
+        board[i][j] = piece.square;
+        this.setBoard(board);
+    }
     // Handles what happens when a square is clicked
     // Resets the data.clicked and data.canMoveTo flags for each square
     // If the square has a piece on it, highlight the piece and it's available moves
     handleClick(i, j) {
-        this.resetBoard();
         if (this.hasPiece(i, j)) {
+            this.resetBoard();
             this.checkMoves(i, j);
             const square = this.getSquareCopy(i, j);
             square.clicked = true;
             this.setSquare(i, j, square);
+        } else if (this.canMoveTo(i, j)) {
+            this.movePieceTo(i, j);
+            this.resetBoard();
+        } else {
+            this.resetBoard();
         }
     }
 
-    hasPiece(i, j) {
-        return this.state.board[i][j].hasPiece;
-    }
     // Checks to see what moves are available for a given piece
     checkMoves(i, j) {
-        const square = this.getSquare(i, j);
-        if (square.piece.isKing) {
-            // TODO: Handle king logic
-        } else {
-            if (square.piece.color === 'red') {
-                if (!this.hasPiece(i + 1, j + 1)) {
-
-                }
-                if (!this.hasPiece(i + 1, j - 1)) {
-
-                }
-            } else {
-                if (!this.hasPiece(i - 1, j + 1)) {
-
-                }
-                if (!this.hasPiece(i - 1, j - 1)) {
-                    
-                }
-            }
-        }
-    }
-
-    // Sets data.clicked and data.canMoveTo to false for every square on the board
-    resetBoard() {
+        const moves = this.getValidMoves(i, j);
         const board = this.getBoardCopy();
-        for (var i=0; i<this.props.rows; i++) {
-            for (var j=0; j<this.props.columns; j++) {
-                board[i][j].clicked = false;
-                board[i][j].canMoveTo = false;
-            }
-        }
+        moves.forEach(move => {
+            board[move.i][move.j].canMoveTo = true;
+        });
         this.setBoard(board);
     }
+
     render() {
         let status = 'Next player: ' + (this.state.blackIsNext ? 'Black' : 'Red');
 
